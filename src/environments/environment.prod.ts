dcd8dc56d80abd9f0dbce354c720d0b1262ebc65
EmmanuelMezaMorales <65479091+edejesus20@users.noreply.github.com>
2023-02-06 14:16:18 -0500

    ultimo commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  // API_URI : 'https://rioprieto.pythonanywhere.com'
+  API_URI : 'https://rioprieto.pythonanywhere.com'
   // API_URI : 'https://rioprieto2022.pythonanywhere.com'
-  API_URI : 'https://www.rioprietoapi.com'
+  // API_URI : 'https://www.rioprietoapi.com'
 
 
 
